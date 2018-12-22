af56934008aa4c5c91b75af5e79611fbe96bebbb
AnshulPatni <anshulpatni19@gmail.com>
2018-12-22 14:57:23 -0800

    done npm init
    Signed-off-by: AnshulPatni <anshulpatni19@gmail.com>

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ console.log('Staring app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes');
 
 var res = notes.add(9, -2);
 console.log(res);
+console.log(_.isString(res));
+var filteredArray = _.uniq(['Anshul', 1, 'Anshul', 2, 3, 4]);
+console.log(filteredArray);
 
 // var user = os.userInfo();
 
