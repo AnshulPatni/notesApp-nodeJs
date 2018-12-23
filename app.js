d59d58ac7cbdb1cd1dce5f7dc8cda7414fe4ad54
AnshulPatni <anshulpatni19@gmail.com>
2018-12-22 19:09:35 -0800

    Refactored code for add notes
    Signed-off-by: AnshulPatni <anshulpatni19@gmail.com>

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ var command = argv._[0];
 console.log('Command: ', command);
 
 if(command === 'add') {
-    notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body);
+    console.log('note:',note);
+    if (!note) {
+        console.log('Note title already in use.')
+    } else {
+        console.log('Note added.')
+        console.log('---');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+    }
 } else if(command === 'list') {
     notes.getAll();
 } else if(command === 'remove') {
